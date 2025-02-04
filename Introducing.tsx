94a3c9885116f39b46f2dadf60b5df86fd257999
decypted <139193041+decypted@users.noreply.github.com>
2025-02-03 21:29:51 -0500

    Add files via upload

---
+++ Introducing.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+const Introducing = () => {
+  return (
+   <div className='py-10'>
+     <div className="mt-20 py-10  w-[100%] mx-auto bg-blck/20  backdrop-blur-xl rounded-3xl">
+      <div className="grid grid-cols-1 md:grid-cols-1 w-11/12 mx-auto items-center gap-20">
+        
+        <div className="grid">
+          <span className="font-bold text-sm text-[#25303b]">Why people like you choose InternMe</span>
+          
+          <h1 className="text-lg md:text-2xl font-normal  mt-5 text-[#25303b] "><span className="font-medium">InternMe</span> is your dedicated teammate in success. We understand that every journey is different, and that&apos;s why we&apos;re here to guide you through the ups and downs, helping you build the future you deserve. Whether you&apos;re chasing your first opportunity, landing your dream job, or seeking to build a career with impact, we&apos;ve got your back.
+
+We do more than just connect people; we empower, guide, and elevate careers. We break barriers, open doors, and make sure you&apos;re not alone on your journey.</h1>
+
+  
+        </div>
+        </div>
+
+      
+    </div>
+   </div>
+  );
+};
+
+export default Introducing;

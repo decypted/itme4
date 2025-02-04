13d9c5f845b513d0c9346fd59b1660d20a96dd7d
decypted <139193041+decypted@users.noreply.github.com>
2025-02-03 21:30:16 -0500

    Add files via upload

--- app/components/HeroSection/SectionDivider/Introducing.tsx
+++ app/components/HeroSection/SectionDivider/Introducing.tsx
@@ -9,7 +9,7 @@ const Introducing = () => {
         <div className="grid">
           <span className="font-bold text-sm text-[#25303b]">Why people like you choose InternMe</span>
           
-          <h1 className="text-lg md:text-2xl font-normal  mt-5 text-[#25303b] "><span className="font-medium">InternMe</span> is your dedicated teammate in success. We understand that every journey is different, and that&apos;s why we&apos;re here to guide you through the ups and downs, helping you build the future you deserve. Whether you're chasing your first opportunity, landing your dream job, or seeking to build a career with impact, we&apos;ve got your back.
+          <h1 className="text-lg md:text-2xl font-normal  mt-5 text-[#25303b] "><span className="font-medium">InternMe</span> is your dedicated teammate in success. We understand that every journey is different, and that&apos;s why we&apos;re here to guide you through the ups and downs, helping you build the future you deserve. Whether you&apos;re chasing your first opportunity, landing your dream job, or seeking to build a career with impact, we&apos;ve got your back.
 
 We do more than just connect people; we empower, guide, and elevate careers. We break barriers, open doors, and make sure you&apos;re not alone on your journey.</h1>
 
